870fda07508a03170aceb83dc93a215d1218c81a
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2015-05-13 18:48:16 +0200

    add template test

--- src/test/scala/com/tomekl007/app/GeeconControllerSpec.scala
+++ src/test/scala/com/tomekl007/app/GeeconControllerSpec.scala
@@ -1,8 +1,19 @@
 package com.tomekl007.app
 
+import akka.actor.ActorSystem
+import com.tomekl007.app.controller.GeeconController
 import org.scalatest.FunSuite
 import org.scalatra.test.scalatest.ScalatraSuite
 
 class GeeconControllerSpec extends FunSuite with ScalatraSuite {
+  implicit val system = ActorSystem()
+
+  addServlet(new GeeconController(), "/*")
+
+  test("") {
+    get(s"path") {
+
+    }
+  }
 
 }
